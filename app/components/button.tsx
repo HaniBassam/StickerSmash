0dd09ef1081f0968b11d2aba5117b480e5f2bac7
HaniB <143654847+HaniBassam@users.noreply.github.com>
2026-04-08 19:33:59 +0200

    Enhance image selection functionality and update Button component to support onPress prop

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -4,9 +4,10 @@ import FontAwesome from "@expo/vector-icons/FontAwesome";
 type Props = {
   label: string;
   theme?: "primary";
+  onPress?: () => void;
 };
 
-export default function Button({ label, theme }: Props) {
+export default function Button({ label, theme, onPress }: Props) {
   if (theme === "primary") {
     return (
       <View
@@ -16,15 +17,8 @@ export default function Button({ label, theme }: Props) {
         ]}
       >
         <Pressable
-          style={[styles.button, { backgroundColor: "#fff" }]}
-          onPress={() => alert("You pressed a button.")}
-        >
-          <FontAwesome
-            name="picture-o"
-            size={18}
-            color="#25292e"
-            style={styles.buttonIcon}
-          />
+          style={[styles.button, { backgroundColor: "#fff" }]} onPressIn={onPress}>
+          <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon}/>
           <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
             {label}
           </Text>
