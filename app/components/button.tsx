f89831f4865b9018aff0f456268845e463caf1f4
HaniB <143654847+HaniBassam@users.noreply.github.com>
2026-04-08 19:04:17 +0200

    Add ImageViewer component and enhance Button with primary theme support

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -1,16 +1,42 @@
-import { StyleSheet, View, Text, Pressable } from "react-native";
+import { StyleSheet, View, Pressable, Text } from "react-native";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 
 type Props = {
     label: string;
+    theme?: "primary";
 };
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+    if (theme === "primary") {
+        return (
+            <View
+                style={[
+                    styles.buttonContainer,
+                    { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 },
+                ]}
+            >
+                <Pressable
+                    style={[styles.button, { backgroundColor: "#fff" }]}
+                    onPress={() => alert("You pressed a button.")}
+                >
+                    <FontAwesome
+                        name="picture-o"
+                        size={18}
+                        color="#25292e"
+                        style={styles.buttonIcon}
+                    />
+                    <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+                </Pressable>
+            </View>
+        );
+    }
+
     return (
-        <view style={styles.buttonContainer}>
-            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
+        <View style={styles.buttonContainer}>
+            <Pressable style={styles.button} onPress={() => alert("You pressed a button.")}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
-        </view>
+        </View>
     );
 }
 
@@ -25,11 +51,14 @@ const styles = StyleSheet.create({
     },
     button: {
         borderRadius: 10,
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
         alignItems: "center",
         justifyContent: "center",
         flexDirection: "row",
+    },
+    buttonIcon: {
+        paddingRight: 8,
     },
     buttonLabel: {
         color: "#ffffff",
