ab7efb702f4568775b064b3c17765d128a256f48
HaniB <143654847+HaniBassam@users.noreply.github.com>
2026-04-08 20:40:43 +0200

    Add CircleButton and IconButton components; enhance Index component with image selection options

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,37 +4,77 @@ import { useState } from "react";
 
 import Button from "@/components/button";
 import ImageViewer from "@/components/ImageViewer";
-
+import CircleButton from "@/components/CircleButton";
+import IconButton from "@/components/IconButton";
 
 const PlaceholderImage = require("../../assets/images/background-image.png");
 
 export default function Index() {
-    const [selectedImage, setSelectedImage] = useState<string | undefined>(
-      undefined,
-    );
-    
-    const pickImageAsync = async () => {
-      let result = await ImagePicker.launchImageLibraryAsync({
-        mediaTypes: ["images"],
-        allowsEditing: true,
-        quality: 1,
-      });
+  const [selectedImage, setSelectedImage] = useState<string | undefined>(
+    undefined,
+  );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
 
-      if (!result.canceled) {
-        setSelectedImage(result.assets[0].uri);
-      } else {
-        alert("You did not select any image.");
-      }
-    };
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+      setShowAppOptions(true);
+    } else {
+      alert("You did not select any image.");
+    }
+  };
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // later to be implemented
+  };
+
+  const onSaveImageAsync = () => {
+    // later to be implemented
+  };
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={selectedImage || PlaceholderImage} />
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
+        />
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-        <Button label="Use this photo" />
-      </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
     </View>
   );
 }
@@ -52,5 +92,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
--- app/components/button.tsx
+++ app/components/button.tsx
@@ -17,7 +17,9 @@ export default function Button({ label, theme, onPress }: Props) {
         ]}
       >
         <Pressable
-          style={[styles.button, { backgroundColor: "#fff" }]} onPressIn={onPress}>
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={onPress}
+        >
           <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon}/>
           <Text style={[styles.buttonLabel, { color: "#25292e" }]}>
             {label}
@@ -31,7 +33,7 @@ export default function Button({ label, theme, onPress }: Props) {
     <View style={styles.buttonContainer}>
       <Pressable
         style={styles.button}
-        onPress={() => alert("You pressed a button.")}
+        onPress={onPress}
       >
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
