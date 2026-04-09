ff9d0327754e2e58d4966263a3db0ae1b55767d5
HaniB <143654847+HaniBassam@users.noreply.github.com>
2026-04-09 18:39:21 +0200

    Add EmojiSticker component for animated sticker display

---
+++ app/components/EmojiSticker.tsx
@@ -0,0 +1,22 @@
+import { ImageSourcePropType, View } from "react-native";
+import { Gesture, GestureDetector } from "react-native-gesture-handler";
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
+
+type Props = {
+    imageSize: number;
+    stickerSource : ImageSourcePropType;
+};
+
+export default function EmojiSticker({ imageSize, stickerSource }: Props) {
+  return (
+    <View style={{ top: -350 }}>
+      <Animated.Image
+        source={stickerSource}
+        resizeMode="contain"
+        style={{ width: imageSize, height: imageSize }}
+      />
+    </View>
+  );
+}
+
+
